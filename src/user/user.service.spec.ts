0a402cf5286b56b6ed9955c9dc5abd1362d309ae
o3ojunseok <o3ojunseok@gmail.com>
2023-02-18 18:16:42 +0900

    [User] unitTest setting

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,37 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepository: UserRepository;
+  const dataSource = {
+    createEntityManager: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        UserRepository,
+        {
+          provide: DataSource,
+          useValue: dataSource
+        },
+        JwtService,
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+  it('should be defined', () => {
+    expect(userRepository).toBeDefined();
   });
 });
