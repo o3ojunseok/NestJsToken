4b4909b3d8e7b59e39436048fce9605a443d8c0b
o3ojunseok <o3ojunseok@gmail.com>
2023-06-23 22:50:31 +0900

    token

--- src/user/jwt.access.strategy.ts
+++ src/user/jwt.access.strategy.ts
@@ -1,14 +1,14 @@
-// import { Injectable, UnauthorizedException } from "@nestjs/common";
-// import { PassportStrategy } from "@nestjs/passport";
-// import { ExtractJwt, Strategy } from 'passport-jwt'
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from 'passport-jwt'
   
-// @Injectable()
-// export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt'){
-//     constructor(){
-//         super({
-//             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//             ignoreExpiration: true,
-//             secretOrKey: 'JWT_ACCESS_KEY',
-//         })
-//     }
-// }
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt'){
+    constructor(){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: true,
+            secretOrKey: 'JWT_ACCESS_KEY',
+        })
+    }
+}
